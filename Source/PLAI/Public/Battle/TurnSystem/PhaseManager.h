52eb73e5a846c324188206c4d3d9eaf9669ace8a
jeongsoll <80992713+jeongsoll@users.noreply.github.com>
2025-04-17 18:06:03 +0900

    폴더링 및 턴제 코드 구현

---
+++ Source/PLAI/Public/Battle/TurnSystem/PhaseManager.h
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameStateBase.h"
+#include "PhaseManager.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PLAI_API AUPhaseManager : public AGameStateBase
+{
+	GENERATED_BODY()
+};
