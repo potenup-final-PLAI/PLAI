4b0dc8cf582d3a1a978b6015d53d4da35c29782b
smk6931 <smk6931@naver.com>
2025-04-27 13:49:30 +0900

    ㄱㄱ

---
+++ Source/PLAI/Item/Npc/NpcCharacter.cpp
@@ -0,0 +1,39 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "NpcCharacter.h"
+
+#include "Components/WidgetComponent.h"
+
+
+// Sets default values
+ANpcCharacter::ANpcCharacter()
+{
+	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	WidgetComp = CreateDefaultSubobject<UWidgetComponent>("Widget");
+	WidgetComp->SetupAttachment(RootComponent);
+	WidgetComp->SetMobility(EComponentMobility::Movable);
+}
+
+// Called when the game starts or when spawned
+void ANpcCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	UiNpc = CreateWidget<UUiNpc>(GetWorld(),UiNpcFactory);
+	WidgetComp->SetWidget(UiNpc);
+	UiNpc->AddToViewport();
+}
+
+// Called every frame
+void ANpcCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+// Called to bind functionality to input
+void ANpcCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+}
+
