0d93cb677c1410a18af548b3c6d35f2649227991
smk6931 <smk6931@naver.com>
2025-05-19 15:57:26 +0900

    미니맵 만드는중

---
+++ Source/PLAI/Item/UI/Inventory/Map/UiWorldMap.cpp
@@ -0,0 +1,19 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "UiWorldMap.h"
+
+#include "Components/Image.h"
+
+void UUiWorldMap::NativeConstruct()
+{
+	Super::NativeConstruct();
+
+	MaterialMapDynamic = UMaterialInstanceDynamic::Create(MaterialMapInterface,this);
+
+	MiniMap->SetBrushFromMaterial(MaterialMapDynamic);
+
+	MaterialMapDynamic->SetScalarParameterValue("ZoomFactor", 1.0f);
+	MaterialMapDynamic->SetVectorParameterValue("CenterOffset", FVector4(0.5f,0.5f,0,0));
+	
+}
---
+++ Source/PLAI/Item/UI/Inventory/Map/UiWorldMap.h
@@ -0,0 +1,29 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "UiWorldMap.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PLAI_API UUiWorldMap : public UUserWidget
+{
+	GENERATED_BODY()
+
+public:
+
+	UPROPERTY(EditAnywhere)
+	UMaterialInterface* MaterialMapInterface;
+	
+	UPROPERTY(EditAnywhere)
+	UMaterialInstanceDynamic* MaterialMapDynamic;
+
+	UPROPERTY(meta = (BindWidget))
+	class UImage* MiniMap;
+
+	virtual void NativeConstruct() override;
+};
